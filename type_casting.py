5a9f04467fce3e048e621781fd55c1c01cf880c4
Rangika-lakshan <lakshanrangika79@gmail.com>
2026-05-02 17:58:09 +0530

    Create type_casting.py

---
+++ type_casting.py
@@ -0,0 +1,16 @@
+#--python casting examples--
+
+#float to integer--
+price = 10.50
+integer_price = int(price)
+print(f"{price} --> casted = {integer_price}")
+
+#integer to float--
+count = 5
+float_count = float(count)
+print(f"{count}--> casted ={float_count}")
+
+#number to complex--
+x = 100
+complex_x = complex(x)
+print(f"{x} --> casted = {complex_x}")
